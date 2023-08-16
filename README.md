56cb24854eabbde4b0c24fc68f8ed48758c7ea21
shamimice03 <shamimice03@gmail.com>
2023-08-16 08:41:36 +0000

    initial

---
+++ README.md
@@ -0,0 +1,32 @@
+# cloud-application-on-aws
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.13 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 4.51.0 |
+
+## Providers
+
+No providers.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_vpc"></a> [vpc](#module\_vpc) | shamimice03/vpc/aws | n/a |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 module "vpc" {
 
-    source = "shamimice03/vpc/aws"
+  source = "shamimice03/vpc/aws"
 
-    vpc_name = "cloud-app-vpc"
-    cidr     = "10.2.0.0/16"
+  vpc_name = "cloud-app-vpc"
+  cidr     = "10.2.0.0/16"
 
-    azs                 = ["ap-northeast-1a", "ap-northeast-1c"]
-    public_subnet_cidr  = ["10.2.0.0/19", "10.2.32.0/19"]
-    private_subnet_cidr = ["10.2.64.0/19", "10.2.96.0/19"]
-    db_subnet_cidr      = ["10.2.128.0/19", "10.2.160.0/19"]
- 
+  azs                 = ["ap-northeast-1a", "ap-northeast-1c"]
+  public_subnet_cidr  = ["10.2.0.0/19", "10.2.32.0/19"]
+  private_subnet_cidr = ["10.2.64.0/19", "10.2.96.0/19"]
+  db_subnet_cidr      = ["10.2.128.0/19", "10.2.160.0/19"]
 
-    enable_dns_hostnames      = true
-    enable_dns_support        = true
-    enable_single_nat_gateway = true
 
-    tags = {
-      "Team" = "devops"
-      "Env"  = "dev"
-    }
+  enable_dns_hostnames      = true
+  enable_dns_support        = true
+  enable_single_nat_gateway = true
+
+  tags = {
+    "Team" = "devops"
+    "Env"  = "dev"
+  }
 }
